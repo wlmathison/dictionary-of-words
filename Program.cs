762c1534cbfac7c0642d1e63ef49cc2ab6bebb39
Billy Mathison <wlmathison@gmail.com>
2019-05-28 13:15:13 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dictionary_of_words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
