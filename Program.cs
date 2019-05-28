e79bb2086fc0597f868db297b9b388f43e4e3ee6
Billy Mathison <wlmathison@gmail.com>
2019-05-28 13:58:12 -0500

    completing list of dictionaries about words exercise

--- Program.cs
+++ Program.cs
@@ -7,6 +7,65 @@ namespace dictionary_of_words
     {
         static void Main(string[] args)
         {
+            // Make a new list
+            List<Dictionary<string, string>> dictionaryOfWords = new List<Dictionary<string, string>>();
+
+            /*
+                You want to track the following about each word:
+                    word, definition, part of speech, example sentence
+
+                Example of one dictionary in the list:
+                {
+                    "word": "excited",
+                    "definition": "having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness",
+                    "part of speech": "adjective",
+                    "example sentence": "I am excited to learn C#!"
+                }
+            */
+
+            // Create dictionary to represent a few word
+            Dictionary<string, string> excitedWord = new Dictionary<string, string>();
+
+            // Add each of the 4 bits of data about the word to the Dictionary
+            excitedWord.Add("word", "excited");
+            excitedWord.Add("definition", "having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness");
+            excitedWord.Add("part of speech", "adjective");
+            excitedWord.Add("example sentence", "I am excited to learn C#!");
+
+            // Add Dictionary to your `dictionaryOfWords` list
+            dictionaryOfWords.Add(excitedWord);
+
+            // create another Dictionary and add that to the list
+            Dictionary<string, string> intriguedWord = new Dictionary<string, string>{
+                { "word", "intrigued" },
+                { "definition", "to strongly draw the interest of; puzzle; fascinate" },
+                { "part of speech", "verb"},
+                { "example sentence", "I am intrigued by C#."}
+            };
+            dictionaryOfWords.Add(intriguedWord);
+
+            /*
+                Iterate your list of dictionaries and output the data
+
+                Example output for one word in the list of dictionaries:
+                    word: excited
+                    definition: having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness
+                    part of speech: adjective
+                    example sentence: I am excited to learn C#!
+            */
+
+            // Iterate the List of Dictionaries
+            foreach (Dictionary<string, string> word in dictionaryOfWords)
+            {
+                // Iterate the KeyValuePairs of the Dictionary
+                foreach (KeyValuePair<string, string> wordData in word)
+                {
+                    Console.WriteLine($"{wordData.Key}: {wordData.Value}");
+                }
+            }
+
+            // *********************************************************************************
+
             /*
                 Create a dictionary with key value pairs to
                 represent words (key) and its definition (value)
